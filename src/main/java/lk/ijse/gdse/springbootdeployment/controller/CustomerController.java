8862cbb42dcbb777e6027cf2372aa47f3a7f7dd9
Dineth0 <dinethnakandala10@gmail.com>
2025-08-07 13:20:05 +0530

    Initial commit

---
+++ src/main/java/lk/ijse/gdse/springbootdeployment/controller/CustomerController.java
@@ -0,0 +1,25 @@
+package lk.ijse.gdse.springbootdeployment.controller;
+
+import lk.ijse.gdse.springbootdeployment.entity.Customer;
+import lk.ijse.gdse.springbootdeployment.service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/customer")
+public class CustomerController {
+    @Autowired
+    private CustomerService customerService;
+
+    @PostMapping("/saveCustomer")
+    public void saveCustomer(@RequestBody Customer customer) {
+        customerService.saveCustomer(customer);
+    }
+
+    @GetMapping("/getAll")
+    public List<Customer> getAllCustomers() {
+       return customerService.getCustomer();
+    }
+}
---
+++ src/main/java/lk/ijse/gdse/springbootdeployment/repo/CustomerRepo.java
@@ -0,0 +1,9 @@
+package lk.ijse.gdse.springbootdeployment.repo;
+
+import lk.ijse.gdse.springbootdeployment.entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerRepo extends JpaRepository<Customer, Integer> {
+}
